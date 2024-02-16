a0f0dc5cc08ba17154c657ce4db566a46b695b50
Dylan Coito <dylanmatthewcoito@gmail.com>
2024-02-15 16:26:04 -0800

    Javascript update.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,61 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+function generatePassword() {
+  var passwordLength = prompt("Enter the length of the password (between 8 and 128):");
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (
+    isNaN(passwordLength) || 
+    passwordLength < 8 ||    
+    passwordLength > 128 ||  
+    !(includeLowercase || includeUppercase || includeNumeric || includeSpecial) 
+  ) {
+    alert("Invalid input. Please try generating a password again.");
+    return null; 
+  }
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  var selectedChars = "";
+
+  if (includeLowercase) {
+    selectedChars += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    selectedChars += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    selectedChars += numericChars;
+  }
+
+  if (includeSpecial) {
+    selectedChars += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * selectedChars.length);
+    password += selectedChars[randomIndex];
+  }
+
+  return password; 
+}
+
+
 generateBtn.addEventListener("click", writePassword);
